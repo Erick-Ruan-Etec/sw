c8cafd67808b91dcb79387ce3fd9c9eb2d93fa5f
Erick Ruan <erickruanetec@gmail.com>
2026-09-18 23:02:25 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,57 @@
+const CACHE_VERSION = "1.0.1";
+
+const FILES = [
+    "/",
+    "/index.html",
+    "/style.css"
+];
+
+self.addEventListener("install", function (event) {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_VERSION).then(function (cache) {
+            return cache.addAll(FILES);
+        })
+    );
+
+});
+
+
+self.addEventListener("activate", function (event) {
+
+    event.waitUntil(
+
+        caches.keys().then(function (keys) {
+
+            return Promise.all(
+
+                keys
+                    .filter(function (key) {
+                        return key !== CACHE_VERSION;
+                    })
+                    .map(function (key) {
+                        return caches.delete(key);
+                    })
+
+            );
+
+        })
+
+    );
+
+});
+
+
+self.addEventListener("fetch", function (event) {
+
+    event.respondWith(
+
+        caches.match(event.request).then(function (response) {
+
+            return response || fetch(event.request);
+
+        })
+
+    );
+
+});
